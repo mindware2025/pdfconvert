02205713a4cf4b1d4ea6aa3c9a6ba008c6ee4c3e
Zaynab Mama <zaynabmamaa@gmail.com>
2025-10-13 22:14:13 +0300

    FIX

--- app.py
+++ app.py
@@ -989,6 +989,7 @@ elif tool == "🟩 Insurance Exposure Tool":
     if uploaded_file:
         output_excel = process_insurance_excel(
             uploaded_file,
+            ageing_filter=True,
             ageing_threshold=ageing_threshold
         )
 
