c389335ec4fba319f89d05c1d75f20db45be73cd
Zaynab Mama <zaynabmamaa@gmail.com>
2025-10-15 17:00:47 +0300

    fix

--- extractors/insurance2.py
+++ extractors/insurance2.py
@@ -38,7 +38,6 @@ def process_grouped_customer_files(file):
                     f"{int(row['Ar Balance']) if pd.notnull(row['Ar Balance']) else ''};"
                     f"{row['Status']};"
                     f"{int(round(row['Paid Amount'])) if pd.notnull(row['Paid Amount']) else ''};"
-                    f"{str(int(round(row['Paid Amount']))) if pd.notnull(row['Paid Amount']) else ''};"
                     f"{row['Payment Date'].strftime('%d/%m/%Y') if pd.notnull(row['Payment Date']) else ''};"
                     f"{row['reason of edd'] if pd.notnull(row['reason of edd']) else ''}",
         axis=1
